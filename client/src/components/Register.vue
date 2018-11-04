9af2fc7dff9a06b8174a7ffbbd48b0382062a2ae
Brian Feister <brian@drm.st>
2018-11-03 22:04:01 -0600

    rough pass at registration form with vue-material, vuelidate, and stubbed API registration endpoint

---
+++ client/src/components/Register.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <form novalidate class="md-layout md-alignment-top-center" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-30 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Register</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field :class="getValidationClass('email')">
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+          </md-field>
+          <md-field :class="getValidationClass('password')">
+            <label for="password">Password</label>
+            <md-input type="password" name="password" id="password" autocomplete="off" v-model="form.password" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+            <span class="md-error" v-else-if="!$v.form.password.password">Invalid password</span>
+          </md-field>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Register</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
+    </form>
+  </div>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+import {
+  required,
+  email,
+  minLength,
+} from 'vuelidate/lib/validators'
+
+export default {
+  name: 'FormValidation',
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      email: null,
+      password: null,
+    },
+    userSaved: false,
+    sending: false,
+    lastUser: null,
+  }),
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    clearForm () {
+      this.$v.$reset()
+      this.form.email = null
+      this.form.password = null
+    },
+    saveUser () {
+      this.sending = true
+
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.lastUser = `${this.form.email}`
+        this.userSaved = true
+        this.sending = false
+        this.clearForm()
+      }, 1500)
+    },
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.saveUser()
+      }
+    }
+  }
+}
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
+  console.log('req.body!', req.body)
   res.send({
-    message: 'hello world'
+    message: `user with email '${req.body.email}' registered with password '${req.body.password}'`
   })
 })
 
